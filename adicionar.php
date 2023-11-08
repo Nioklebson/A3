5535991818a3b349958b2018b6e618b3c95a3e85
Nioklebson <146145299+Nioklebson@users.noreply.github.com>
2023-11-07 21:22:49 -0300

    Add files via upload

--- adicionar.php
+++ adicionar.php
@@ -1,4 +1,61 @@
-<!DOCTYPE html>
+ <?php
+
+ session_start();
+
+ include('conexao.php');
+include ('funcoes.php');
+ $nome = isset ($_POST['nome']) ? $_POST['nome'] : '';
+ $cpf = isset ($_POST['cpf']) ? $_POST['cpf'] : '';
+ $telefone = isset ($_POST['telefone']) ? $_POST['telefone'] : '';
+ $login = isset ($_POST['login']) ? $_POST['login'] : '';
+ $senha = isset ($_POST['senha']) ? $_POST['senha'] : '';
+
+ $selectcpf = "SELECT cpf FROM 
+              usuario WHERE cpf = '$cpf'";
+ $querycpf = mysqli_query($conexao,$selectcpf);
+ $dadocpf = mysqli_fetch_row($querycpf);
+ 
+ $selectlogin = "SELECT login FROM
+              login WHERE login = '$login'";
+ $querylogin = mysqli_query($conexao,$selectlogin);
+ $dadologin = mysqli_fetch_row($querylogin); 
+ 
+ if ($nome <> NULL) {
+   if (($dadocpf == NULL ) && 
+    ($dadologin == NULL)){
+
+        $insertusuario = "INSERT INTO usuario (nome, cpf , telefone)
+        VALUES
+        ('$nome','$cpf', '$telefone')";
+        $queryusuario = 
+            mysqli_query($conexao,$insertusuario);
+            $senhacriptografada = criptografar($senha);
+
+            $insertlogin = "INSERT INTO
+             login (cpf , login , senha, nivel)
+            VALUES
+            ('$cpf', '$login', '$senhacriptografada',3)";
+            $querylogin = mysqli_query($conexao,$insertlogin);
+            echo '<script>alert("Usuário cadastrados com sucesso");
+			window.location="adicionar.php";
+			</script>';
+
+     } else {
+
+            echo '<script>alert("CPF e/
+        ou login já cadastrados no sistema");
+			window.location="adicionar.php";
+			</script>';
+         }
+    }
+
+
+
+
+?>
+ 
+ <!DOCTYPE html>
+
 <html>
 <head>
 	<meta charset="utf-8">
@@ -7,13 +64,14 @@
 </head>
 <body>
     <center>
+        <h1>Adiconar Usuário </h1>
         <form id="form-add"
         action="#" method="POST">
         Nome: <input type text="text" name="nome"><br>
         CPF: <input type="text"name="cpf"><br>
         Telefone: <input type="text" name="telefone"><br>
         Login: <input type="text"name="login"><br>
-        Senha: <input type="text"name="password" name="senha"><br>
+        Senha: <input type="password" name="senha"><br>
         <input type="submit"name="cadastrar"value="cadastrar">
 </form>
 </center>
